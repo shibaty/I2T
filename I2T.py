717859c6b650c4502787ed175ff85cd5da6f2b70
shibaty <shibaty@gmail.com>
2020-05-05 13:34:29 +0900

    update python3.8

--- I2T.py
+++ I2T.py
@@ -3,7 +3,6 @@
 """ Instagram to Twitter """
 
 import os
-import sys
 import urllib.request
 import shutil
 import time
@@ -18,111 +17,113 @@ DOWNLOAD_FILE_PATH = '/tmp/'
 
 
 def download_medias(urls):
-    """donload medias"""
-    paths = []
-    for count, url in enumerate(urls):
-        if count >= config.CONFIG['TWITTER_MEDIAS_MAX']:
-            break
-        filename = url.rsplit('/', 1)[1].split('?')[0]
-        path = DOWNLOAD_FILE_PATH + filename
-        with urllib.request.urlopen(url) as data, open(path, 'wb') as file:
-            shutil.copyfileobj(data, file)
-        paths.append(path)
-    return paths
+  """donload medias"""
+  paths = []
+  for count, url in enumerate(urls):
+    if count >= config.CONFIG['TWITTER_MEDIAS_MAX']:
+      break
+    filename = url.rsplit('/', 1)[1].split('?')[0]
+    path = DOWNLOAD_FILE_PATH + filename
+    with urllib.request.urlopen(url) as data, open(path, 'wb') as file:
+      shutil.copyfileobj(data, file)
+    paths.append(path)
+  return paths
 
 
 def cleanup_medias(paths):
-    """cleanup downloaded medias"""
-    for path in paths:
-        os.remove(path)
+  """cleanup downloaded medias"""
+  for path in paths:
+    os.remove(path)
 
 
 def get_instagram_recent_post(interval):
-    """get Instagram recent post"""
+  """get Instagram recent post"""
 
-    insta = InstagramClient(config.CONFIG['INSTAGRAM_ACCESS_TOKEN'],
-                            config.CONFIG['INSTAGRAM_CLIENT_SECRET'])
+  insta = InstagramClient(config.CONFIG['INSTAGRAM_ACCESS_TOKEN'],
+                          config.CONFIG['INSTAGRAM_CLIENT_SECRET'])
 
-    media = insta.get_recent_media()
+  media = insta.get_recent_media()
 
-    created_time = media.get_created_time()
-    now_time = datetime.utcnow()
-    delta = now_time - created_time
+  created_time = media.get_created_time()
+  now_time = datetime.utcnow()
+  delta = now_time - created_time
 
-    if int(delta.total_seconds()) >= interval + 10:
-        return False, "", "", []
+  if int(delta.total_seconds()) >= interval + 10:
+    return False, "", "", []
 
-    caption = media.get_caption()
-    link = media.get_link()
-    urls = media.get_media_urls()
+  caption = media.get_caption()
+  link = media.get_link()
+  urls = media.get_media_urls()
 
-    print("get instagram Caption: " + caption)
+  print("get instagram Caption: " + caption)
 
-    return True, caption, link, urls
+  return True, caption, link, urls
 
 
 def post_twitter(caption, link, paths):
-    """post twitter"""
+  """post twitter"""
 
-    print("post twitter Caption: " + caption)
+  print("post twitter Caption: " + caption)
 
-    twit = TwitterClient(config.CONFIG['TWITTER_CONSUMER_KEY'],
-                         config.CONFIG['TWITTER_CONSUMER_SECRET'],
-                         config.CONFIG['TWITTER_ACCESS_TOKEN_KEY'],
-                         config.CONFIG['TWITTER_ACCESS_TOKEN_SECRET'])
+  twit = TwitterClient(config.CONFIG['TWITTER_CONSUMER_KEY'],
+                       config.CONFIG['TWITTER_CONSUMER_SECRET'],
+                       config.CONFIG['TWITTER_ACCESS_TOKEN_KEY'],
+                       config.CONFIG['TWITTER_ACCESS_TOKEN_SECRET'])
 
-    media_ids = []
-    media_videos_count = 0
-    for path in paths:
-        mime_type = mimetypes.guess_type(path)[0]
-        if 'video' in mime_type:
-            media_videos_count = media_videos_count + 1
-        else:
-            media_ids.append(twit.media_upload(path, mime_type))
+  media_ids = []
+  media_videos_count = 0
+  for path in paths:
+    mime_type = mimetypes.guess_type(path)[0]
+    if 'video' in mime_type:
+      media_videos_count = media_videos_count + 1
+    else:
+      media_ids.append(twit.media_upload(path, mime_type))
 
-    media_ids_len = len(media_ids)
-    count = 1
-    count_max = 0
-    count_message = ""
+  media_ids_len = len(media_ids)
+  count = 1
+  count_max = 0
+  count_message = ""
 
-    if media_ids_len > 0:
-        if media_videos_count > 0:
-            count_max = 1 + media_videos_count
-            count_message = str(count) + "/" + str(count_max) + " "
-            count = count + 1
-        message = (count_message + caption)[0:140]
-        twit.post_with_medias(message, media_ids)
+  if media_ids_len > 0:
+    if media_videos_count > 0:
+      count_max = 1 + media_videos_count
+      count_message = str(count) + "/" + str(count_max) + " "
+      count = count + 1
+    message = (count_message + caption)[0:140]
+    twit.post_with_medias(message, media_ids)
 
-    for path in paths:
-        mime_type = mimetypes.guess_type(path)[0]
-        if 'video' in mime_type:
-            count_message = str(count) + "/" + str(count_max) + " "
-            message = (count_message + caption)[0:140]
-            media_video_id = twit.media_upload(path, mime_type)
-            twit.post_with_medias(message, {media_video_id})
-            count = count + 1
+  for path in paths:
+    mime_type = mimetypes.guess_type(path)[0]
+    if 'video' in mime_type:
+      count_message = str(count) + "/" + str(count_max) + " "
+      message = (count_message + caption)[0:140]
+      media_video_id = twit.media_upload(path, mime_type)
+      twit.post_with_medias(message, {media_video_id})
+      count = count + 1
+
 
 def get_and_post(interval):
-    """get instagram recent post and post twitter"""
-    try:
-        ret, caption, link, urls = get_instagram_recent_post(interval)
-        if ret:
-            paths = download_medias(urls)
-            post_twitter(caption, link, paths)
-            cleanup_medias(paths)
-    except IOError:
-        print("Network unreachable?")
+  """get instagram recent post and post twitter"""
+  try:
+    ret, caption, link, urls = get_instagram_recent_post(interval)
+    if ret:
+      paths = download_medias(urls)
+      post_twitter(caption, link, paths)
+      cleanup_medias(paths)
+  except IOError:
+    print("Network unreachable?")
+
 
 def main_routine():
-    """main routine"""
-    interval = config.CONFIG['INTERVAL_SECONDS']
+  """main routine"""
+  interval = config.CONFIG['INTERVAL_SECONDS']
 
-    print("I2T INTERVAL:" + str(interval) + " secs")
-    while True:
-        get_and_post(interval)
-        time.sleep(interval)
+  print("I2T INTERVAL:" + str(interval) + " secs")
+  while True:
+    get_and_post(interval)
+    time.sleep(interval)
+
 
 # main
 if __name__ == '__main__':
-    main_routine()
-
+  main_routine()
--- TwitterClient.py
+++ TwitterClient.py
@@ -8,71 +8,71 @@ import twitter
 
 
 class TwitterClient(object):
-    """Twitter Client"""
+  """Twitter Client"""
 
-    def __init__(self, con_key, con_secret, token, token_secret):
-        """Constructor"""
-        self.auth = twitter.OAuth(token, token_secret, con_key, con_secret)
-        self.api = twitter.Twitter(auth=self.auth)
-        self.upload_api = twitter.Twitter(
-            domain="upload.twitter.com", auth=self.auth)
+  def __init__(self, con_key, con_secret, token, token_secret):
+    """Constructor"""
+    self.auth = twitter.OAuth(token, token_secret, con_key, con_secret)
+    self.api = twitter.Twitter(auth=self.auth)
+    self.upload_api = twitter.Twitter(
+        domain="upload.twitter.com", auth=self.auth)
 
-    def post(self, message):
-        """Post Message"""
-        self.api.statuses.update(status=message)
+  def post(self, message):
+    """Post Message"""
+    self.api.statuses.update(status=message)
 
-    def media_upload(self, path, mime_type):
-        """Media Upload"""
-        file_size = os.path.getsize(path)
-        media_category = 'TweetImage'
-        if 'video' in mime_type:
-            media_category = 'tweet_video'
-        media_id = self.upload_api.media.upload(
-            command='INIT', media_type=mime_type,
-            media_category=media_category,
-            total_bytes=file_size)['media_id_string']
+  def media_upload(self, path, mime_type):
+    """Media Upload"""
+    file_size = os.path.getsize(path)
+    media_category = 'TweetImage'
+    if 'video' in mime_type:
+      media_category = 'tweet_video'
+    media_id = self.upload_api.media.upload(
+        command='INIT', media_type=mime_type,
+        media_category=media_category,
+        total_bytes=file_size)['media_id_string']
 
-        with open(path, 'rb') as file:
-            seg_id = 0
-            bytes_sent = 0
-            while bytes_sent < file_size:
-                # Chunk size is less than 5M bytes
-                chunk = file.read(4 * 1024 * 1024)
-                self.upload_api.media.upload(
-                    command='APPEND',
-                    media_id=media_id,
-                    media=chunk,
-                    segment_index=seg_id)
-                bytes_sent = file.tell()
-                seg_id = seg_id + 1
+    with open(path, 'rb') as file:
+      seg_id = 0
+      bytes_sent = 0
+      while bytes_sent < file_size:
+        # Chunk size is less than 5M bytes
+        chunk = file.read(4 * 1024 * 1024)
+        self.upload_api.media.upload(
+            command='APPEND',
+            media_id=media_id,
+            media=chunk,
+            segment_index=seg_id)
+        bytes_sent = file.tell()
+        seg_id = seg_id + 1
 
-        finalize_result = self.upload_api.media.upload(
-            command='FINALIZE',
-            media_id=media_id)
+    finalize_result = self.upload_api.media.upload(
+        command='FINALIZE',
+        media_id=media_id)
 
-        if 'processing_info' in finalize_result:
-            processing_info = finalize_result['processing_info']
-            while processing_info['state'] != 'succeeded':
-                if processing_info['state'] == 'failed':
-                    return ""
+    if 'processing_info' in finalize_result:
+      processing_info = finalize_result['processing_info']
+      while processing_info['state'] != 'succeeded':
+        if processing_info['state'] == 'failed':
+          return ""
 
-                time.sleep(processing_info['check_after_secs'])
+        time.sleep(processing_info['check_after_secs'])
 
-                status_result = self.upload_api.media.upload(
-                    command='STATUS',
-                    media_id=media_id,
-                    _method='GET')
-                if not 'processing_info' in status_result:
-                    break
-                processing_info = status_result['processing_info']
+        status_result = self.upload_api.media.upload(
+            command='STATUS',
+            media_id=media_id,
+            _method='GET')
+        if 'processing_info' not in status_result:
+          break
+        processing_info = status_result['processing_info']
 
-        return media_id
+    return media_id
 
-    def post_with_medias(self, message, media_ids):
-        """Post Message with Medias"""
-        self.api.statuses.update(
-            media_ids=self.__list2csv(media_ids), status=message)
+  def post_with_medias(self, message, media_ids):
+    """Post Message with Medias"""
+    self.api.statuses.update(
+        media_ids=self.__list2csv(media_ids), status=message)
 
-    @classmethod
-    def __list2csv(cls, items):
-        return ",".join(items)
+  @classmethod
+  def __list2csv(cls, items):
+    return ",".join(items)
